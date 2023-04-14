0dbf1f5002aa16bf61ae8277d0513a86bc7c491c
MeatyMage46 Canu <canu.leonardo@itismeucci.com>
2023-04-14 11:48:15 +0200

    Tentativo di risoluzione angular material non andato a buon fine

--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee, ServerData } from '../types/Employee';
 import { EmployeeService } from '../services/employee.service';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-my-table',
@@ -8,25 +9,28 @@ import { EmployeeService } from '../services/employee.service';
   styleUrls: ['./my-table.component.css']
 })
 export class MyTableComponent implements OnInit {
-
+  //ATTRIBUTI============================================================================================
   displayedColumns: string[] = ['id', 'birthDate', 'firstName', 'lastName', 'gender', 'hireDate'];
 
   data: ServerData | undefined;
-  dataSource: Employee[] | undefined;
-
+  dataSource: MatTableDataSource<Employee>;
+  
+  //CONSTRUCTOR==========================================================================================
   constructor ( private employeeService: EmployeeService){
     this.loadData("http://localhost:8080/employees");
-    this.dataSource = this.data?._embedded.employees;
+    this.dataSource = new MatTableDataSource(this.data?._embedded.employees);
   }
+  //=====================================================================================================
 
+  //METHODS==============================================================================================
   loadData(url: string){
     this.employeeService.getData(url).subscribe(
       serverResponse => {
         this.data = serverResponse;
-        this.dataSource = this.data?._embedded.employees;
+        this.dataSource.data = this.data?._embedded.employees;
       }
     )
   }
-
+  //=====================================================================================================
   ngOnInit(): void {}
 }
